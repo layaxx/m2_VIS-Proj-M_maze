1100ea0e078c1db645da2f56afb245a6906da33d
Yannick Lang <layaxx@gmx.de>
2023-11-06 00:19:33 +0100

    WIP: add player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    private float movementSpeed = 2f;
+    private float rotationSpeed = 120f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Move player with WASD keys
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0, 0, verticalInput) * movementSpeed * Time.deltaTime);
+        transform.Rotate(new Vector3(0, horizontalInput * rotationSpeed * Time.deltaTime, 0));
+        // transform.rotation = Quaternion.Euler(0, 0, 0);
+
+
     }
 }
