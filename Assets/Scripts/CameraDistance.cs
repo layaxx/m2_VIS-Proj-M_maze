be1b31a78c96ee25110b2089afac86a58fdf6632
Yannick Lang <layaxx@gmx.de>
2023-11-11 00:51:52 +0100

    add scroll wheel interaction for camera distance

---
+++ Assets/Scripts/CameraDistance.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    private float progress = 0.15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position);
+
+        Debug.Log(transform.localPosition);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxis("Mouse ScrollWheel") * 50;
+
+        //move towards the center of the world (or where ever you like)
+        progress = Mathf.Clamp(progress + speed * Time.deltaTime, 0.05f, 1);
+
+        transform.localPosition = new Vector3(0, 01f, -3f) + (3 * progress * new Vector3(0, 4.5f, 1f));
+        transform.localRotation = Quaternion.Euler(5 + (70 * progress), 0, 0);
+
+    }
+}
